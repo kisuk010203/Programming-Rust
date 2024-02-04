ddd7178755aea53347eff5e6fbcec34b5d294742
kisuk010203 <chriskisuk@gmail.com>
2024-02-04 23:29:18 +0900

    Run Rust Formatter

--- Chapter 2/hello/src/main.rs
+++ Chapter 2/hello/src/main.rs
@@ -2,9 +2,9 @@ use std::io::Write;
 use std::str::FromStr;
 fn main() {
     let mut numbers = Vec::new();
-    for arg in std::env::args().skip(1) { // first value is the name of the program, so we skip it
-        numbers.push(u64::from_str(&arg)
-            .expect("error parsing argument"));
+    for arg in std::env::args().skip(1) {
+        // first value is the name of the program, so we skip it
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
     }
     if numbers.len() == 0 {
         writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
@@ -30,8 +30,8 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 }
 
 #[test] // This is an attribute. It is a way to add metadata to items like functions, modules, and crates.
-fn test_gcd(){
+fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
     assert_eq!(gcd(2, 5), 1);
-    assert_eq!(gcd(3*5*11*17, 3*7*11*13), 3*11);
+    assert_eq!(gcd(3 * 5 * 11 * 17, 3 * 7 * 11 * 13), 3 * 11);
 }
--- Chapter 2/iron-gcd/src/main.rs
+++ Chapter 2/iron-gcd/src/main.rs
@@ -1,5 +1,6 @@
 extern crate iron;
-#[macro_use] extern crate mime;
+#[macro_use]
+extern crate mime;
 extern crate router;
 extern crate urlencoded;
 
@@ -21,26 +22,28 @@ fn get_form(_request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
     response.set_mut(status::Ok);
     response.set_mut(mime!(Text/Html; Charset=Utf8));
-    response.set_mut(r#"
+    response.set_mut(
+        r#"
         <title>GCD Calculator</title>
         <form action="/gcd" method="post">
             <input type="text" name="n"/>
             <input type="text" name="n"/>
             <button type="submit">Compute GCD</button>
         </form>
-    "#);
+    "#,
+    );
     Ok(response)
 }
 fn post_gcd(request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
-    
+
     let form_data = match request.get_ref::<UrlEncodedBody>() {
         Err(e) => {
             response.set_mut(status::BadRequest);
             response.set_mut(format!("Error parsing form data: {:?}\n", e));
             return Ok(response);
         }
-        Ok(map) => map
+        Ok(map) => map,
     };
     println!("form_data: {:?}", form_data);
     let unparsed_numbers = match form_data.get("n") {
@@ -49,18 +52,23 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
             response.set_mut(format!("form data has no 'n' parameter\n"));
             return Ok(response);
         }
-        Some(nums) => nums
+        Some(nums) => nums,
     };
-    
+
     let mut numbers = Vec::new();
     for unparsed in unparsed_numbers {
         match u64::from_str(&unparsed) {
             Err(_) => {
                 response.set_mut(status::BadRequest);
-                response.set_mut(format!("Value for 'n' parameter not a number: {:?}\n", unparsed));
+                response.set_mut(format!(
+                    "Value for 'n' parameter not a number: {:?}\n",
+                    unparsed
+                ));
                 return Ok(response);
             }
-            Ok(n) => { numbers.push(n); }
+            Ok(n) => {
+                numbers.push(n);
+            }
         }
     }
     let mut d = numbers[0];
@@ -69,9 +77,11 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
     }
     response.set_mut(status::Ok);
     response.set_mut(mime!(Text/Html; Charset=Utf8));
-    response.set_mut(format!("The greatest common divisor of the numbers {:?} is <b>{}</b>\n", numbers, d));
+    response.set_mut(format!(
+        "The greatest common divisor of the numbers {:?} is <b>{}</b>\n",
+        numbers, d
+    ));
     Ok(response)
-    
 }
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
