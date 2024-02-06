639f429b50c4f496591838903b81878e2e595dbe
kisuk010203 <chriskisuk@gmail.com>
2024-02-07 00:32:33 +0900

    Implement Mandelbrot Program using parallel structures

--- Chapter 2/mandelbrot/src/main.rs
+++ Chapter 2/mandelbrot/src/main.rs
@@ -1,6 +1,12 @@
+extern crate crossbeam;
+extern crate image;
 extern crate num;
-use num::{complex::ParseComplexError, Complex};
-use std::{path::Prefix, str::FromStr};
+use image::png::PNGEncoder;
+use image::ColorType;
+use num::Complex;
+use std::fs::File;
+use std::io::Write;
+use std::str::FromStr;
 
 /// Parse the string `s` as a coordinate pair, like `"400X600"` or `"1.0,0.5"`.
 /// Specifically, `s` should have the form <left><sep><right>, where <sep> is the character given by the `separator` argument,
@@ -48,16 +54,75 @@ fn pixel_to_point(
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
 ) -> Complex<f64> {
-    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
-    let x = Complex{re: 2.1, im:3.2};
-    x
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
 }
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex { re: -0.5, im: -0.5 }
+    );
+}
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one grayscale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on the complex plane corresponding to the upper-left
+/// and lower-right corners of the pixel bugger.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the file named `filename`.
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    // The return type on success is the unit type (), which is kind of like `void`.
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
+    Ok(())
+}
+#[allow(dead_code)]
 fn square_loop(mut x: i64) {
     loop {
         // This is an infinite loop.
         x = x * x;
     }
 }
+#[allow(dead_code)]
 fn square_add_loop(c: f64) {
     let mut x = 0.;
     loop {
@@ -87,8 +152,48 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 fn main() {
-    let c = Complex { re: 0.1, im: 0.3 };
-    let limit = 100;
-    let escape = escape_time(c, limit);
-    println!("Is {:?} a Mandelbrot number? {:?}", c, escape);
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(
+            std::io::stderr(),
+            "Usage: mandelbrot FILE PIXLES UPPERLEFT LOWERRIGHT"
+        )
+        .unwrap();
+        writeln!(
+            std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1.0/20",
+            args[0]
+        )
+        .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    // println!("bounds: {:?}, upper_left: {:?}, lower_right: {:?}", bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
