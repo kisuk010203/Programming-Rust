a1fb7d45157a9017bc7ad182c8f0a5bbcbbe8c77
kisuk010203 <chriskisuk@gmail.com>
2024-02-07 15:22:54 +0900

    Run `clean` before pushing.

--- Chapter 3/nested_array/src/main.rs
+++ Chapter 3/nested_array/src/main.rs
@@ -13,7 +13,7 @@ fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
 /// let v = [3, 4];
 /// assert_eq!(math_ex::magnitude(v), 5.0);
 /// ```
-fn magnitude(v : &Vec<f64>) -> f64 {
+fn magnitude(v: &Vec<f64>) -> f64 {
     let mut sum = 0.0;
     for coord in v {
         sum += coord * coord;
@@ -33,12 +33,12 @@ fn normalize(v: &Vec<f64>) -> Vec<f64> {
         result.push(coord / mag);
     }
     result
-}   
+}
 fn main() {
     let matrix = [[101, 102, 103], [201, 202, 203], [301, 302, 303]];
     let v = vec![5.0, 12.0];
     println!("Original Matrix: {:?}", matrix);
     println!("Transposed Matrix: {:?}", transpose(matrix));
-    println!("Magnitude of v : {}"  , magnitude(&v));
+    println!("Magnitude of v : {}", magnitude(&v));
     println!("Normalized v : {:?}", normalize(&v));
 }
